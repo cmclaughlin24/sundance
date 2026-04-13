985d25d1594d375aa32aad523d283536af736c90
cmclaughlin24 <curtismclaughlin91@gmail.com>
2026-04-12 22:19:14 -0500

    set-up forms service bootstrap

--- services/forms/internal/core/core.go
+++ services/forms/internal/core/core.go
@@ -2,11 +2,16 @@ package core
 
 import (
 	"log"
+	"os"
 
+	"github.com/cmclaughlin24/sundance/forms/internal/adapters/persistence"
 	"github.com/cmclaughlin24/sundance/forms/internal/core/ports"
+	"github.com/cmclaughlin24/sundance/forms/internal/core/services"
 )
 
-type ApplicationSettings struct{}
+type ApplicationSettings struct {
+	Persistence persistence.PersistanceSettings `json:"persistence"`
+}
 
 type Application struct {
 	Logger     *log.Logger
@@ -14,6 +19,25 @@ type Application struct {
 	repository *ports.Repository
 }
 
-func NewApplication(settings *ApplicationSettings) (*Application, error) {
-	return nil, nil
+func NewApplication(settings ApplicationSettings) (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	r, err := persistence.Bootstrap(settings.Persistence, logger)
+
+	if err != nil {
+		return nil, err
+	}
+
+	s := services.Bootstrap(logger, r)
+
+	return &Application{
+		Logger:     logger,
+		Services:   s,
+		repository: r,
+	}, nil
+}
+
+func (app *Application) Close() {
+	if err := app.repository.Database.Close(); err != nil {
+		log.Fatalf("an error occurred while closing the database connection: %s", err.Error())
+	}
 }
