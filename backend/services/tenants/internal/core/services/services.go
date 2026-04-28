6f4e9cd4740adc615092d3989a8aa0f9a5acb136
cmclaughlin24 <curtismclaughlin91@gmail.com>
2026-04-28 05:33:48 -0500

    bug fixes

--- backend/services/tenants/internal/core/services/services.go
+++ backend/services/tenants/internal/core/services/services.go
@@ -6,12 +6,10 @@ import (
 	"github.com/cmclaughlin24/sundance/backend/pkg/common/stratreg"
 	"github.com/cmclaughlin24/sundance/backend/services/tenants/internal/core/domain"
 	"github.com/cmclaughlin24/sundance/backend/services/tenants/internal/core/ports"
-	"github.com/cmclaughlin24/sundance/backend/services/tenants/internal/core/strategies"
 )
 
 func Bootstrap(logger *log.Logger, repository *ports.Repository) *ports.Services {
-	lookupStrategies := stratreg.New[domain.DataSourceType, LookupStrategy]().
-		Set(domain.DataSourceTypeStatic, strategies.NewStaticLookupStrategy())
+	lookupStrategies := stratreg.New[domain.DataSourceType, LookupStrategy]()
 
 	return &ports.Services{
 		Tenants:     NewTenantsService(logger, repository),
